c46c1bfc20bd260ca0608a262ba8ef67fd513956
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2024-07-01 10:12:52 -0700

    Initial Commit

---
+++ MyCam/MyCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCamApp.swift
+//  MyCam
+//
+//  Created by 김하연 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
